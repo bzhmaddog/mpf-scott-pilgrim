65061f2a128c169e7428f8de414640246312d3ef
Cedric Martin <cedmart1@gmail.com>
2022-09-25 10:28:08 +0200

    Fixes for Chromium 105

--- mc/www/js/renderers/ScoreEffectRenderer.mjs
+++ mc/www/js/renderers/ScoreEffectRenderer.mjs
@@ -95,9 +95,9 @@ class ScoreEffectRenderer {
                             }
                             @group(0) @binding(0) var<storage,read> noisePixels: Image;
                             @group(0) @binding(1) var<storage,read> inputPixels: Image;
-                            @group(0) @binding(2) var<storage,write> outputPixels: Image;
+                            @group(0) @binding(2) var<storage,read_write> outputPixels: Image;
 
-                            @stage(compute)
+                            @compute
                             @workgroup_size(1)
                             fn main (@builtin(global_invocation_id) global_id: vec3<u32>) {
                                 let index : u32 = global_id.x + global_id.y * ${that.#width}u;
