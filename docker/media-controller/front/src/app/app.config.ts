a61fbd5e732a6da20faa6da9b0223d6bb6eae64d
Cédric Martin <cedmart1@gmail.com>
2026-05-14 11:08:46 +0200

    chore: update Docker configuration and Angular settings

--- docker/media-controller/front/src/app/app.config.ts
+++ docker/media-controller/front/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import {ApplicationConfig, provideZonelessChangeDetection} from '@angular/core';
+import {ApplicationConfig} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
@@ -10,7 +10,6 @@ import {BaseMode} from "@mpf/modes/base-mode";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZonelessChangeDetection(),
     provideRouter(routes),
     provideResourcesManager({
       file: 'resources.json',
