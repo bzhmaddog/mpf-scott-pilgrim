9fe54e221aaae297f91a00157812580970289b66
Cédric Martin <cedric.martin@open-groupe.com>
2026-06-09 17:20:10 +0200

    feat(store): refactor game store methods and computed properties into separate files

---
+++ docker/media-controller/front/src/app/store/game.store.computed.ts
@@ -0,0 +1,10 @@
+import {withComputed} from "@ngrx/signals";
+import {computed} from "@angular/core";
+
+export function withGameStoreComputed() {
+  return withComputed(({players, player}) => ({
+    currentPlayerState: computed(() => players()[player() - 1]),
+    currentPlayerScore: computed(() => players()[player() - 1]?.score ?? 0),
+    currentPlayerBall:  computed(() => players()[player() - 1]?.ball ?? 0),
+  }));
+}
